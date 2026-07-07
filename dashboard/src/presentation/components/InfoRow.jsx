cf29dad5a928c33de2d174c51d3801cbf7a37215
SantosRojas <santos777rojas@gmail.com>
2026-07-07 11:31:12 -0500

    change to dashboard-ts

--- dashboard/src/presentation/components/InfoRow.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const InfoRow = ({ label, value, gridColumn, labelStyle, valueStyle }) => (
-  <div style={{ gridColumn }}>
-    <span style={{ color: 'var(--text-tertiary)', display: 'block', fontSize: 'var(--fs-xxs)', ...labelStyle }}>{label}</span>
-    <span style={{ fontWeight: 500, ...valueStyle }}>{value}</span>
-  </div>
-);
